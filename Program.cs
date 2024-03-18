41535dd624dce38098da149526c81acba82e3ee5
relhe <renellherisson@gmail.com>
2024-03-18 18:12:53 -0400

    Small user input in console

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		string name;
+		string firstName;
+		Console.Write("Enter your first name: ");
+		firstName = Console.ReadLine();
+		Console.Write("Enter your last name: ");
+		name = Console.ReadLine();
+		Console.WriteLine($"Hello {firstName} {name}! you are welcome to C# programming.");
+	}
+}
